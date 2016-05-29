0741a5601bdca1fc18c691916351b7ec07162516
Ruijie Zhang <jerrydream123@gmail.com>
2016-05-29 13:31:50 -0700

    finished

--- app.js
+++ app.js
@@ -10,20 +10,58 @@ myApp.config(function($routeProvider) {
          templateUrl: 'pages/weather.html',
          controller: 'weatherController'
     })
+    .when('/weather/:days', {
+         templateUrl: 'pages/weather.html',
+         controller: 'weatherController'
+    })
 });
 
 // services
-myApp.service('cityService', )
+myApp.service('cityService', function(){
+    this.city="San Francisco, CA";
+})
 
 
 // controllers
-myApp.controller('mainController', ['$scope', function($scope){
+myApp.controller('mainController', ['$scope','cityService', function($scope, cityService){
+    $scope.city = cityService.city;
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    })
     console.log('loading main controller...');
 }]);
 
-myApp.controller('weatherController', ['$scope', function($scope){
+myApp.controller('weatherController', ['$scope',  '$resource','$routeParams' ,'cityService', function($scope, $resource, $routeParams, cityService){
     console.log('loading weather controller...');
+    $scope.city = cityService.city;
+    
+    $scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=b4b33a0e922f906fb393d3b81c5b1b13", 
+                                  {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+    
+    $scope.days = $routeParams.days || 2;
+    $scope.weatherResult = $scope.weatherApi.get({q:$scope.city, cnt: $scope.days});
+    
+    
+    
+    $scope.convertToK = function(degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    }
+    $scope.convertDate = function(dt) {
+        return new Date(dt * 1000);
+    }
+    console.log($scope.weatherResult);
 }]);
 
-
-
+myApp.directive("weatherReport", function() {
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/weatherReport.html',
+       replace: true,
+       scope: {
+           weatherDay: "=",
+           convertToStandard: "&",
+           convertToDate: "&",
+           dateFormat: "@"
+       }
+   }
+});
